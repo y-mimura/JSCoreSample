3db55bd537b33c6d062109c02575f46c8ff24421
y-mimura <yuuya.mimura@uis-inf.co.jp>
2019-03-04 22:07:41 +0900

    Initial Commit

---
+++ JSCoreSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSCoreSample
+//
+//  Created by 三村裕矢 on 2019/03/04.
+//  Copyright © 2019 Yuya Mimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
